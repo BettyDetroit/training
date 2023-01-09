a9cea3a98d24c847d76a07befcf5e8d567330146
BettyDetroit <caubelnelly@gmail.com>
2023-01-09 16:41:58 +0100

    condition ternaire modifiée

--- src/Country.js
+++ src/Country.js
@@ -5,31 +5,20 @@ import React, { useState, useEffect } from "react";
 const Country = () => {
   const [data, setData] = useState([]);
   const [selectRegion, setSelectRegion] = useState("");
- console.log(selectRegion, 'mappage')
-  const continent = [
-    "",
-    "Europe",
-    "Africa",
-    "Americas",
-    "Asia",
-    "Oceania",
-  ];
+  console.log(selectRegion, "mappage");
+  const continent = ["", "Europe", "Africa", "Americas", "Asia", "Oceania"];
 
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => setData(res.data));
   }, []);
-  console.log(data)
+  console.log(data);
   return (
     <div className="pays">
-
-      <select 
-            onChange={(e) => setSelectRegion(e.target.value)}
-            >
-            {continent.map((item, index) => (
-            <option key={index}
-            value={item}>
+      <select onChange={(e) => setSelectRegion(e.target.value)}>
+        {continent.map((item, index) => (
+          <option key={index} value={item}>
             {item}
           </option>
         ))}
@@ -37,7 +26,9 @@ const Country = () => {
 
       <ul>
         {data
-          .filter((country) => !data ? (""): (country.region.includes(selectRegion)))
+          .filter((country) =>
+            selectRegion ? country.region.includes(selectRegion) : true
+          )
           .map((country, index) => (
             <Card key={index} country={country} />
           ))}
@@ -47,5 +38,3 @@ const Country = () => {
 };
 
 export default Country;
-
-
